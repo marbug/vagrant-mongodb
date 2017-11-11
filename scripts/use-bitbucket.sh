9a13a528f6ac6b7dcca898536e5804b9b4c56c57
Maryan Bagnyuk <marbuga@gmail.com>
2017-11-11 13:53:53 +0200

    add scripts

---
+++ scripts/use-bitbucket.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git remote -v
+git remote set-url origin git@bitbucket.org:marbug/vagrant-mongodb.git
+git remote -v
---
+++ scripts/use-github.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git remote -v
+git remote set-url origin git@github.com:marbug/vagrant-mongodb.git
+git remote -v
