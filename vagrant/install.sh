2cff2404172ad15fa8311a11a36098f7095f2feb
Maryan Bagnyuk <marbuga@gmail.com>
2017-11-11 15:49:37 +0200

    enhance install.sh

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -2,4 +2,14 @@
 
 set -e
 
-echo "==== installing mongodb ..."
+echo "==== import public key ..."
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+
+echo "==== create a list file ..."
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+
+echo "==== reload packages ..."
+sudo apt-get update
+
+echo "==== install mongodb"
+sudo apt-get install -y mongodb-org
